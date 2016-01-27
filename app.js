9694b0dd7af9495364ceb25aaaff6b40fb05c2fa
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-01-27 20:19:53 +0530

    Refactored graphs
    Refactored graphs; Graphs is the submodule required by the library. The
    graphs is maintained by other git repository.

--- app.js
+++ app.js
@@ -2,19 +2,13 @@ var fs = require('fs');
 
 var data = fs.readFileSync('./data/All_Routes_By_Number.txt','utf-8');;
 
-var graphLib = require('./lib/graph.js');
-
 var commandLineArgs = require('command-line-args');
-
-var Graph = graphLib.WeightedGraph;
-
-var Edge = graphLib.Edge;
 
 var lib = require('./lib/lib.js');
 
 var refinedBusData = lib.refineBusData(data);
 
-var graph =  lib.makeGraph(Graph, Edge, refinedBusData);
+var graph =  lib.makeGraph(refinedBusData);
 
 var cli = commandLineArgs([
   { name: 'route', alias: 'r', type: Boolean },
@@ -22,15 +16,16 @@ var cli = commandLineArgs([
   { name: 'direct', alias: 'd', type: Boolean },
   { name: 'help', alias: 'h', type: Boolean },
   { name: 'from', alias: 'f', type: String, multiple: true },
-  { name: 'to', alias: 't', type: String, multiple: true}
+  { name: 'to', alias: 't', type: String, multiple: true},
+  { name: 'station', alias: 's', type: String, multiple: true}
 ]);
 
 var options = cli.parse();
 
 if(options.route){
-	console.time();
+	console.time('route');
 	console.log(graph.shortestPath(options.from.join(" "),options.to.join(" ")));
 	console.timeEnd();
 }
 
-// console.log(options);
+console.log(options);
